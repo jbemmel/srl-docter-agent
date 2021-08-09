1be2df82bae150c82679fac9247d20ed6bad3474
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-08-09 17:17:54 -0500

    Cleanup

--- srl-x-connect-parts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CLAB="/home/jeroen/srlinux/containerlab/containerlab"
-
-sudo $CLAB tools veth create --debug -a clab-son-lab-spine1:e1-2 -b clab-son-lab-leaf2:e1-1
-sudo $CLAB tools veth create --debug -a clab-son-lab-spine2:e1-1 -b clab-son-lab-leaf1:e1-2
