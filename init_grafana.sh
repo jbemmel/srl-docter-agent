1deb172afb18e4fbaa49eff50c592c2cad6a3c87
Alejo Guevara <jaguevara@gmail.com>
2021-08-10 11:11:02 -0400

    Update init_grafana.sh

--- init_grafana.sh
+++ init_grafana.sh
@@ -9,7 +9,7 @@ docker restart clab-srl-docter-lab-grafana
 sleep 3
 
 for f in grafana_dashboard_all.json ; do
-curl -u 'admin:admin' "http://${GRAFANA}:3000/api/dashboards/import" -X POST -H 'Content-Type: application/json;charset=UTF-8' -d "@./dashboards/${f}"
+curl -u 'admin:ZkaLIRBb0EOfn9wxBN2P' "http://${GRAFANA}:3000/api/dashboards/import" -X POST -H 'Content-Type: application/json;charset=UTF-8' -d "@./dashboards/${f}"
 done
 
 exit 0
