c357a5491d1b751f8f02b2be7b6a6579f5f9d539
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-08-09 23:04:54 -0500

    Init script

---
+++ init_grafana.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GRAFANA="172.20.20.10"
+
+docker container exec clab-srl-docter-lab-grafana grafana-cli plugins install flant-statusmap-panel
+docker container exec clab-srl-docter-lab-grafana grafana-cli plugins install agenty-flowcharting-panel
+docker restart clab-srl-docter-lab-grafana
+
+sleep 3
+
+for f in grafana_dashboard_all.json ; do
+curl -u 'admin:admin' "http://${GRAFANA}:3000/api/dashboards/import" -X POST -H 'Content-Type: application/json;charset=UTF-8' -d "@./dashboards/${f}"
+done
+
+exit 0
+
