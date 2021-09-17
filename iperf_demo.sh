aa981d76e19b207430aec70397492047852264d7
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-09-17 09:39:15 -0500

    Add simple iperf demo script

---
+++ iperf_demo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker exec -it clab-srl-docter-lab-client1 apk add iperf
+docker exec -it clab-srl-docter-lab-client2 apk add iperf
+
+# Run iperf as UDP daemon on client2
+docker exec -it clab-srl-docter-lab-client2 iperf -s -u -D
+
+# Connect from client1, 100 kbps
+docker exec -it clab-srl-docter-lab-client1 iperf -c 10.10.10.12 -u --time 300 --bandwidth 100k
