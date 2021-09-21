a139c68b128a2f70b90ef86c0daa3a89f4920426
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-09-21 09:33:34 -0500

    Reduce intervals for demo

--- iperf_demo.sh
+++ iperf_demo.sh
@@ -7,4 +7,4 @@ docker exec -it clab-srl-docter-lab-client2 apk add iperf
 docker exec -it clab-srl-docter-lab-client2 iperf -s -u -D
 
 # Connect from client1, 100 kbps
-docker exec -it clab-srl-docter-lab-client1 iperf -c 10.10.10.12 -u --time 300 --bandwidth 100k
+docker exec -it clab-srl-docter-lab-client1 iperf -c 10.10.10.12 -u --time 120 --bandwidth 100k
