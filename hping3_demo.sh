ad3c9db60e57eb60d1ec4599c504cf663b4f9009
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-09-21 17:18:35 -0500

    Add hping3

---
+++ hping3_demo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker exec -it clab-srl-docter-lab-client3 apk add hping1 --update-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing
+
+# Ping SRL from client1, 10 pps
+docker exec -it clab-srl-docter-lab-client1 hping3 10.10.10.1 --icmp -c 100 --fast
