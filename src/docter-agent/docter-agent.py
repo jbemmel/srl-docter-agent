e9847b604cd4e2a0e17ce942a0d3d2b7cf372cbb
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-09-21 12:23:33 -0500

    str

--- src/docter-agent/docter-agent.py
+++ src/docter-agent/docter-agent.py
@@ -125,7 +125,7 @@ def Update_Metric(ts_ns, metric, contributor, contrib_status, updates=[], sla=No
                       ]
                  return sorted(vals,reverse=True)[:5] # Top 5
                else:
-                 pid_cpu = re.match( ".*srl_nokia-platform-cpu:process\\[pid=([0-9]+)\\]/cpu-utilization=([0-9]+)$")
+                 pid_cpu = re.match( ".*srl_nokia-platform-cpu:process\\[pid=([0-9]+)\\]/cpu-utilization=([0-9]+)$", data )
                  if pid_cpu:
                      _gs = pid_cpu.groups()
                      _pid = int( _gs[0] )
