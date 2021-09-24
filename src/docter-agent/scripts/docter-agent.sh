f02a394c8d8195dd13e228a17ae651081c644457
Jeroen van Bemmel <jeroen.van_bemmel@nokia.com>
2021-09-24 11:07:35 -0500

    Use new basedir

--- src/docter-agent/scripts/docter-agent.sh
+++ src/docter-agent/scripts/docter-agent.sh
@@ -17,7 +17,7 @@ function main()
 {
     trap _term SIGTERM
     local virtual_env="/opt/srlinux/python/virtual-env/bin/activate"
-    local main_module="/opt/srlinux/agents/docter-agent/docter-agent.py"
+    local main_module="/opt/demo-agents/docter-agent/docter-agent.py"
 
     # source the virtual-environment, which is used to ensure the correct python packages are installed,
     # and the correct python version is used
